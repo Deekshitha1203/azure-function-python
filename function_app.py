947477b8395faa9b2c2bd091d0281cfe4eebd526
Deekshitha1203 <deekshithasuddamandu@gmail.com>
2026-04-07 15:39:03 +0530

    Create function_app.py

---
+++ function_app.py
@@ -0,0 +1,12 @@
+import azure.functions as func
+import logging
+ 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+ 
+@app.route(route="hello")
+def hello(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    name = req.params.get('name')
+    if not name:
+        name = "World"
+    return func.HttpResponse(f"Hello, {name}! Function App is working!")
